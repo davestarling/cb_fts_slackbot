32db44625a100493c9cfc00650a20f65fb0fcb1c
Dave Starling <dave@seenit.io>
2017-03-26 09:56:08 +0100

    Configuration files for development environment

---
+++ conf/logging/development.py
@@ -0,0 +1,26 @@
+LOG_CONF = {
+    'version': 1,
+
+    'formatters': {
+        'void': {
+            'format': ''
+        },
+        'standard': {
+            'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+        },
+    },
+    'handlers': {
+        'default': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard',
+            'stream': 'ext://sys.stdout'
+        }
+    },
+    'loggers': {
+        '': {
+            'handlers': ['default'],
+            'level': 'DEBUG'
+        }
+    }
+}
